fef986626f907fb32bfe073e9b4e8239e37cd5ae
remokasu <0w0.ebi.kaitai@gmail.com>
2026-06-03 00:21:03 +0900

    不要パッチ削除

--- Core.cs
+++ Core.cs
@@ -47,7 +47,7 @@ namespace BakuonOfflinePatch
     {
         public const string PLUGIN_GUID = "com.bakuon.offlinepatch";
         public const string PLUGIN_NAME = "BakuonOfflinePatch";
-        public const string PLUGIN_VERSION = "1.0.4";
+        public const string PLUGIN_VERSION = "1.0.5";
     }
 
     /// <summary>
