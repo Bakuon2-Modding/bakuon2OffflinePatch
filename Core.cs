3619c020f8c9c5429c4b9871eecf860e85fc2fa5
remokasu <0w0.ebi.kaitai@gmail.com>
2026-06-23 20:03:02 +0900

    チャットの診断用ログを削除

--- Core.cs
+++ Core.cs
@@ -47,7 +47,7 @@ namespace BakuonOfflinePatch
     {
         public const string PLUGIN_GUID = "com.bakuon.offlinepatch";
         public const string PLUGIN_NAME = "BakuonOfflinePatch";
-        public const string PLUGIN_VERSION = "1.0.8";
+        public const string PLUGIN_VERSION = "1.0.9";
     }
 
     /// <summary>
